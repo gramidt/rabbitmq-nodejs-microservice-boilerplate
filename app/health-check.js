69c1636d5d4a9a5c214c45decdc19ee4ce8546fa
Granville Schmidt <granville.schmidt@yahoo.com>
2016-02-24 23:00:16 -0500

    added logging for when health endpoint has started

--- app/health-check.js
+++ app/health-check.js
@@ -1,9 +1,12 @@
 const Kardia = require('kardia');
 const Config = require('./services/ConfigurationService');
+const logger = require('./services/LogService');
 
 const healthAppName = Config.get('HEALTH_ENDPOINT_NAME') || 'some-worker-service-change-me';
-const healthCheckHost = Config.get('HEALTH_ENDPOINT_HOST') || '0.0.0.0';
+const healthHost = Config.get('HEALTH_ENDPOINT_HOST') || '0.0.0.0';
 const healthPort = Config.get('HEALTH_ENDPOINT_PORT') || 12900;
 
 // Start up the health check endpoint
-Kardia.start({ name: healthAppName, host: healthCheckHost, port: healthPort });
+Kardia.start({ name: healthAppName, host: healthHost, port: healthPort });
+
+logger.info(`Kardia started at ${healthHost}:${healthPort} with service name of ${healthAppName}`);
